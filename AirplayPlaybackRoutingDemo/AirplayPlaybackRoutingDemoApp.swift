f9fe5931261df37707838bfc65a3ac52f07cd9db
itsuki <itsuki.enjyoy@gmail.com>
2025-11-01 08:56:52 +0900

    Initial Commit

---
+++ AirplayPlaybackRoutingDemo/AirplayPlaybackRoutingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirplayPlaybackRoutingDemoApp.swift
+//  AirplayPlaybackRoutingDemo
+//
+//  Created by Itsuki on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct AirplayPlaybackRoutingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
